0a902eec6feb2fbb863c955f2f6b39904c204982
Keishaa <keishaavarma@gmail.com>
2020-06-29 22:11:19 +0530

    Error

--- plinko.js
+++ plinko.js
@@ -3,7 +3,8 @@ class plinko{
         var options = {
             'restitution':0.8,
             'friction':1.0,
-            'density':1.0
+            'density':1.0,
+            'isStatic':true
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
@@ -13,9 +14,8 @@ class plinko{
       display(){
         push();
         translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
         rectMode(CENTER);
-        rect(this.image, 0, 0, this.width, this.height);
+        rect(0, 0, this.width, this.height);
         pop();
       }
 }
--- sketch.js
+++ sketch.js
@@ -1,36 +1,54 @@
 var ground, div1, div2, div3;
+var divisionHeight= 100
 var particles=[];
 var plinkos=[];
 var divisions=[];
 var divHeight=300;
+var rand; 
+const Bodies= Matter.Bodies;
+const Engine= Matter.Engine;
+const World= Matter.World;
 
 function setup() {
   createCanvas(800,400);
-  //createSprite(400, 200, 50, 50);
+  engine= Engine.create();
+  world= engine.world;
   ground = new Ground(400,390,800,5);
-
+  for (var k=0; k<=800; k+=80){
+    divisions.push(new Division(k, 400-divisionHeight/2, 10, divisionHeight));
+  }
+  console.log(divisions);
+  for (var j= 40; j<=800; j+=50){
+    for (var k=75; k<=275; k+=50){
+    plinkos.push(new plinko(j,k,20,20));
+    }
+  }
+  /*for (var j= 15; j<=800; j+=50){
+    plinkos.push(new plinko(j,175));
+  }*/
 }
 
 function draw() {
   background("black"); 
   ground.display();
-  for (var k=0; k<=800; k+=80){
-    divisions.push(new divisions(k, 400-divisionHeight/2, 10, divisionHeight));
+
+  Engine.update(engine);
+
+  for (var m=0; m<particles.length; m++){
+    rand= Math.round(random(0,800))
+    particles.push(new particle(m,rand,10,10));
   }
 
-  for (var j= 40; j<=800; j+=50){
-    plinkos.push(new plinko(j,75));
+  for (var n=0; n<particles.length; n++){
+    particles[n].display();
   }
 
-  for (var j= 15; j<=800; j+=50){
-    plinkos.push(new plinko(j,175));
+  for (var j=0; j<plinkos.length; j++){
+    plinkos[j].display();
+
   }
 
-  for (var j=0; j<particles.length; j++){
-    particles[j].display();
-  }
-
-  for (var k= 0; k<particles.length; k++){
+  for (var k= 0; k<divisions.length; k++){
     divisions[k].display();
   }
   /*div1.display();
